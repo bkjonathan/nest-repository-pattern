155143b0e6e278d0e0fa1575a767c0ebccbd67ce
bkjonathan <kyawhtetsan.jar1990@gmail.com>
2023-12-18 19:28:22 +0630

    Add TypeORM and Create Mysql Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3308,
+      username: 'root',
+      password: 'RootPwd',
+      database: 'nest_test',
+      entities: [],
+      synchronize: true
     })
   ],
   controllers: [],
